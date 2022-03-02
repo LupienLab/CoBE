c55d3e3148de3944b44f1fbff13d1586fd722888
nandankita <nandankita@gmail.com>
2022-03-02 00:42:56 -0500

    Update runbwa.sh

--- GSI_WGS/runbwa.sh
+++ GSI_WGS/runbwa.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
+#SBATCH -p himem 
 #SBATCH --job-name=Bwa_Mem
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=8
+#SBATCH --ntasks-per-node=10
 #SBATCH --time=168:00:00
-#SBATCH --mem=10gb
+#SBATCH --mem=50gb
 #SBATCH --output=BwaMem.%J.out
 #SBATCH --error=BwaMem.%J.err
 
 module load bwa/0.7.15
 
-bwa mem hg38 FILE1_R1.fq.gz FILE1_R2.fq.gz -t $SLURM_NTASKS_PER_NODE > bwa_mem_alignments.sam
+echo "read1" $1
+echo "read2" $2
+
+
+bwa mem genome/hg38 $1 $2 -t $SLURM_NTASKS_PER_NODE > bwa_mem_alignments.sam
